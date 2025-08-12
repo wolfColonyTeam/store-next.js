47789b84f97387a491402a8ab4dbb7ff1699ff7d
serhiikovalenko <kovalenkoserg1990@gmail.com>
2025-08-12 22:34:14 +0300

    base template

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer bg-black p-3 text-white text-center">
+      <div>Footer title</div>
+      <div>Footer nav</div>
+    </footer>
+  );
+}
