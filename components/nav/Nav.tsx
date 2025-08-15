9c326b20e5bee429452df8fe70bc4b7790cae55f
ilumm <furt.kariina@gmail.com>
2025-08-15 16:49:29 +0300

    update nav

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -3,25 +3,41 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 export default function Nav() {
+  const navList = [
+    {
+      name: "Home",
+      href: "/",
+    },
+    {
+      name: "About",
+      href: "/about",
+    },
+    {
+      name: "Contacts",
+      href: "/contacts",
+    },
+  ];
+
   return (
     <nav className="bg-black p-4 text-white flex justify-between">
       <div className="flex gap-2">
-        <Button asChild>
-          <Link href="/">Home</Link>
-        </Button>
-        <Button asChild>
-          <Link href="/about">About</Link>
-        </Button>
-        <Button asChild>
-          <Link href="/contacts">Contacts</Link>
-        </Button>
+        {navList.map((link: { name: string; href: string }) => {
+          return (
+            <Button asChild>
+              <Link key={link.name} href={link.href}>
+                {link.name}
+              </Link>
+            </Button>
+          );
+        })}
       </div>
+
       <div className="flex gap-2">
         <Button asChild>
           <Link href="/login">Login</Link>
         </Button>
         <Button asChild>
-          <Link href="/sign-up">Sign Up</Link>
+          <Link href="/sign">Sign Up</Link>
         </Button>
       </div>
     </nav>
