6f2db86481ed3738b36a207549e955d93954586d
ilumm <furt.kariina@gmail.com>
2025-09-06 11:04:45 +0300

    add ts, delete route

--- components/categories/createCategory.tsx
+++ components/categories/createCategory.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import {createCategoryAction} from "@/actions";
 
-interface inputsDataI {
-    type: string,
-    name: string,
-    placeholder: string,
-    className: string,
-    label: string,
-    id: string,
-}
-
 function CreateCategory() {
     const inputsData = [
         {
             type: "text",
             name: "name",
             placeholder: "title",
-            className: "border px-3 py-2 rounded",
             label: "Title",
             id: "title",
         },
@@ -24,7 +14,6 @@ function CreateCategory() {
             type: "text",
             name: "description",
             placeholder: "Description",
-            className: "border px-3 py-2 rounded",
             label: "Description",
             id: "description",
         },
@@ -32,17 +21,21 @@ function CreateCategory() {
             type: "text",
             name: "tag",
             placeholder: "Tag",
-            className: "border px-3 py-2 rounded",
             label: "Tag",
             id: "tag",
-        },
-    ]
+        }
+    ] as const;
+
+    // type InputData = typeof inputsData[number];
+    // type FormValues = {
+    //     [K in InputData["name"]]: string;
+    // };
 
     return (
         <div className={"border p-5 rounded-2xl bg-white w-sm"}>
             <h3 className={'mb-3'}>Create category</h3>
             <form action={createCategoryAction} className={"flex flex-col space-y-2 max-w-sm text-small-text"}>
-                {inputsData.map((item: inputsDataI) => (
+                {inputsData.map((item) => (
                     <React.Fragment key={item.id}>
                         <label htmlFor="title">
                             {item.label}
@@ -52,7 +45,7 @@ function CreateCategory() {
                             id={item.id}
                             name={item.name}
                             placeholder={item.placeholder}
-                            className={item.className}
+                            className={"border px-3 py-2 rounded"}
                             required
                         />
                     </React.Fragment>
