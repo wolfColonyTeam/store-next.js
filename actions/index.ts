8f47480be58796df14a8afa73870fd1d0a759842
serhiikovalenko <kovalenkoserg1990@gmail.com>
2025-09-10 03:01:18 +0300

    clear unused

--- actions/index.ts
+++ actions/index.ts
@@ -1,8 +1,5 @@
 "use server";
 import { signIn, signOut } from "@/auth";
-import connectDb from "@/lib/dbConnect";
-import Category from "@/model/category.model";
-
 
 export const logoutAction = async () => {
   await signOut({ redirectTo: "/login" });
@@ -13,15 +10,3 @@ export const socialLogin = async (formData: FormData) => {
   await signIn(String(action), { redirectTo: "/" });
   console.log(action, " action");
 };
-
-export const createCategoryAction = async (formData: FormData) => {
-  await connectDb();
-
-  const name = formData.get("name");
-  const description = formData.get("description");
-  const tag = formData.get("tag");
-
-  const newCategory = await Category.create({name, tag, description})
-
-  console.log('New category created:', newCategory);
-}
